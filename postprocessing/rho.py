98cb2fea177ed1b90f3582b2a7598e54f29de7e2
ctmbl <mabileau.clement@gmail.com>
2023-03-17 10:42:01 +0100

    Update rho to plot file passed as first parameter

--- postprocessing/rho.py
+++ postprocessing/rho.py
@@ -7,8 +7,13 @@ import sys
 from ester import *
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
+if len(sys.argv) == 1:
+    print("Please give a file to plot as 1sr param")
+    exit(1)
 # the input file is here M5_O95
-a=star2d('star.h5')
+a=star2d(sys.argv[1])
 plt.plot(a.r[:],np.log10(a.rho[:]))
+plt.plot(a.r[:],np.log10(a.T[:]))
 plt.show()
